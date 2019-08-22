0c8d467c92da138a82d871bab89203b12f5a66f8
Daniel <517397241@qq.com>
2019-08-22 22:53:26 +0800

    completed registered

--- flask/application/apps/index/views.py
+++ flask/application/apps/index/views.py
@@ -19,9 +19,8 @@ def register():
         return render_template("register.html")
 
 
-# 路由实现动态传参
-# 需求：使用一个视图函数来显示不同用户的订单信息
-@index_blu.route("/order/<int:order_id>")  # <>定义路由参数， <>内需要起个名字
-# 需要在函数()内填入参数名，name后面的代码就能够使用
-def get_order_id(order_id):
-    return "order_id is %s" %order_id
+@index_blu.route("/home/", methods=['GET'])
+def home():
+    return render_template("home.html")
+
+
